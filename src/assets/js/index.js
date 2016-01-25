6ad6b1389730824059455abc423d04735374d97c
Mitchell Gates <md.fragala@gmail.com>
2016-01-25 15:29:24 -0500

    Add fadeins.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -7,12 +7,14 @@ const page = document.querySelector('.page');
 const navigation = document.querySelector('.navbar');
 const overlay = document.querySelector('.page__overlay');
 const toggle = document.querySelector('.navbar__toggle');
+const lines = document.querySelectorAll('.navbar__toggleLine');
 const menu = document.querySelector('.navMenu');
 const heroCarousel = document.querySelector('.heroCarousel');
 
 const pageState = state(page);
 const overlayState = state(overlay);
 const toggleState = state(toggle);
+const linesState = state(lines);
 const menuState = state(menu);
 
 const navHeadroom = new Headroom(navigation, {
@@ -23,12 +25,14 @@ navHeadroom.init();
 toggle.addEventListener('click', function(e) {
   overlayState.toggle('active');
   toggleState.toggle('active');
+  linesState.toggle('active');
   menuState.toggle('active');
   pageState.toggle('locked');
 });
 overlay.addEventListener('click', function(e) {
   overlayState.set('active', false);
   toggleState.set('active', false);
+  linesState.set('active', false);
   menuState.set('active', false);
   pageState.set('locked', false);
 });
@@ -36,6 +40,7 @@ window.addEventListener('resize', function(e) {
   if (window.innerWidth >= 700 && menuState.get('active')) {
     overlayState.set('active', false);
     toggleState.set('active', false);
+    linesState.set('active', false);
     menuState.set('active', false);
     pageState.set('locked', false);
   }
