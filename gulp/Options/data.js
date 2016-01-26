50b877d6c3ed01219cea07a1e1586ca14447eada
Mitchell Gates <md.fragala@gmail.com>
2016-01-26 16:02:19 -0500

    Refactor HTML as SPA.

--- gulp/Options/data.js
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-module.exports = function(file) {
-	return {
-		filename: path.basename(file.path, '.html')
-	}
-}
--- gulp/swig/locals/app.js
+++
@@ -1,9 +0,0 @@
-import configs from '../../configure';
-import Path from 'path';
-
-const build = configs.paths.root.build;
-module.exports = function(path) {
-	let trim = path.replace(/$\.\//g, '');
-	let build = Path.resolve(configs.paths.root.build);
-	return path;
-}
