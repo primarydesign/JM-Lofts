1d3b53f7a3f8c93fade5555befbb6dd20aa6b3ff
Mitchell Gates <md.fragala@gmail.com>
2016-01-26 16:39:08 -0500

    Recode welcome slider.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,5 @@
 import tillstand from './library/tillstand';
 import velocity from './vendors/velocity.js';
 
-import './modules/navbar';
-import 'imports?jQuery=jquery!./modules/hero-carousel';
+import './modules/navbar.js';
+// import 'imports?jQuery=jquery!./modules/hero-carousel';
