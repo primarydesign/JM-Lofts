bc459729230326769b67e070590a9890252bad66
Mitchell Gates <md.fragala@gmail.com>
2016-01-29 17:43:12 -0500

    Setup Google Maps.

--- src/assets/js/modules/downtown-map.js
+++ src/assets/js/modules/downtown-map.js
@@ -1,5 +1,20 @@
+import ascript from 'scriptjs';
 import locations from '../../_data/locations';
 import tillstand from '../library/tillstand';
+import Mapster from '../library/mapster';
 import './categories';
 
+ascript("https://maps.googleapis.com/maps/api/js?v=3", function() {
+  const mapster = Mapster(google);
+  const Downtown = new mapster('.locationsMap__map', {
+    center: { lat: 39.833333, lng: -98.583333 },
+    mapTypeControl: false,
+    maxZoom: 15,
+    minZoom: 3,
+    scrollwheel: false,
+    streetViewControl: false,
+    zoom: 5,
+    zoomControlOptions: { position: google.maps.ControlPosition.TOP_LEFT },
+  });
 
+});
