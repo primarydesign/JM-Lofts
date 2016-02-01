b7a09a02b9ee75c7c01b174b62a95d6477616927
Mitchell Gates <md.fragala@gmail.com>
2016-02-01 16:10:34 -0500

    Add basic scrolling.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,15 @@
+import scry from './library/scry';
+
 import './modules/navbar';
 import './modules/romance-captions';
 import './modules/ft-carousel';
 import './modules/downtown-map';
+
+const scryTriggers = document.querySelectorAll('[data-scry]');
+for(let i = 0; i < scryTriggers.length; i++) {
+  scryTriggers[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    scry(this);
+  });
+}
 
--- src/assets/js/library/scry.js
+++ src/assets/js/library/scry.js
@@ -1,7 +1,9 @@
 import Velocity from '../vendors/velocity';
 
 export default function(element, options = {}) {
+  console.log('scrying');
+  let target = document.querySelector(element.getAttribute('data-scry'));
   if (element instanceof HTMLElement) {
-    Velocity(element, "scroll", options);
+    Velocity(target, "scroll", options);
   }
 }
