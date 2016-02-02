0884acc8a416f62d1e90802e9a6ea204e95d000f
Mitchell Gates <md.fragala@gmail.com>
2016-02-02 17:25:44 -0500

    Link featuresList bullets to slide index.

--- src/assets/js/modules/ft-carousel.js
+++ src/assets/js/modules/ft-carousel.js
@@ -1,9 +1,17 @@
+import tillstand from '../library/tillstand';
 import '../vendors/glide.js';
+
+const bullets = tillstand(document.querySelectorAll('.featureList__item'));
+bullets.tillstand.instate('active');
+
+console.log(bullets[0].tillstand);
 
 jQuery('.featuresCarousel').glide({
   type: 'carousel',
   autoplay: false,
   animationDuration: 400,
+  afterInit: highlighBullets,
+  afterTransition: highlighBullets,
   classes: {
     base: 'featuresCarousel',
     wrapper: 'featuresCarousel__frame',
@@ -11,3 +19,12 @@ jQuery('.featuresCarousel').glide({
     slide: 'featuresCarousel__item'
   }
 });
+
+function highlighBullets(event) {
+  let feature = event.current.data('content');
+  for(let i = 0; i < bullets.length; i++) {
+    let token = bullets[i].getAttribute('data-feature');
+    let match = Boolean(token === feature);
+    bullets[i].tillstand.active.set(match);
+  }
+}
