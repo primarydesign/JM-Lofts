667f2d46be4445aac41017b2930e389e16a3b7ef
Mitchell Gates <md.fragala@gmail.com>
2016-02-03 11:20:04 -0500

    Add production Features photos. Remove Features carousel bullet highlighting.

--- src/assets/js/modules/ft-carousel.js
+++ src/assets/js/modules/ft-carousel.js
@@ -1,17 +1,9 @@
-import tillstand from '../library/tillstand';
 import '../vendors/glide.js';
-
-const bullets = tillstand(document.querySelectorAll('.featureList__item'));
-bullets.tillstand.instate('active');
-
-console.log(bullets[0].tillstand);
 
 jQuery('.featuresCarousel').glide({
   type: 'carousel',
   autoplay: false,
   animationDuration: 400,
-  afterInit: highlighBullets,
-  afterTransition: highlighBullets,
   classes: {
     base: 'featuresCarousel',
     wrapper: 'featuresCarousel__frame',
@@ -19,12 +11,3 @@ jQuery('.featuresCarousel').glide({
     slide: 'featuresCarousel__item'
   }
 });
-
-function highlighBullets(event) {
-  let feature = event.current.data('content');
-  for(let i = 0; i < bullets.length; i++) {
-    let token = bullets[i].getAttribute('data-feature');
-    let match = Boolean(token === feature);
-    bullets[i].tillstand.active.set(match);
-  }
-}
