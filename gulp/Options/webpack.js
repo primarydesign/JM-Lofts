f342032d835021361d4dd384a82456d08f7eeb1a
Mitchell Gates <md.fragala@gmail.com>
2016-02-10 11:03:09 -0500

    Optimixe scripts.

--- gulp/Options/webpack.js
+++ gulp/Options/webpack.js
@@ -5,10 +5,38 @@ import path from 'path';
 module.exports = {
   devtool: 'source-map',
   plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      compress: {
+        sequences: true,
+        properties: true,
+        dead_code: false,
+        drop_debugger: false,
+        unsafe: false,
+        conditionals: false,
+        comparisons: false,
+        evaluate: false,
+        booleans: false,
+        loops: true,
+        unused: true,
+        hoist_funs: true,
+        hoist_vars: false,
+        if_return: true,
+        join_vars: true,
+        cascade: false,
+        warnings: false,
+        negate_iife: false,
+        pure_getters: false,
+        pure_funcs: null,
+        drop_console: true,
+        keep_fargs: true,
+        keep_fnames: false,
+      }
+    }),
     new webpack.ProvidePlugin({
       jQuery: 'jquery'
     })
-    ],
+  ],
   resolve: {
     root: [path.join(__dirname, 'node_modules')],
   },
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function() {
   require('./modules/navbar');
   require('./modules/romance-captions');
+  require('./modules/unit-table');
   require('./modules/ft-carousel');
   require('./modules/downtown-map');
   require('./modules/contact-form');
-  require('./modules/unit-table');
 });
