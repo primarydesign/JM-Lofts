320a44526fb9b2711ce6df713c02be4d2f106b70
Mitchell Gates <md.fragala@gmail.com>
2016-02-26 13:03:22 -0500

    Build map menu appearance behavior.

--- src/assets/js/modules/downtown-map.js
+++ src/assets/js/modules/downtown-map.js
@@ -1,116 +1,35 @@
-// import tillstand from '../library/tillstand';
+import moptions from '../library/moptions';
 import Mapster from '../library/mapster';
-import Controls from './categories';
-import '../vendors/slim-scroll';
+import velocity from '../vendors/velocity';
 
-// const Interface = new Controls('.mapUI');
-const JMLCenter = { "lat": 42.773403, "lng": -71.083941 };
-const Downtown = new Mapster('.mapCanvas__frame', {
-  center: JMLCenter,
-  mapTypeControl: false,
-  maxZoom: 18,
-  minZoom: 3,
-  scrollwheel: false,
-  streetViewControl: false,
-  zoom: 16,
-  zoomControlOptions: { position: google.maps.ControlPosition.TOP_LEFT },
-  styles: [{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"color":"#d78a3a"},{"gamma":"1.61"},{"lightness":"10"},{"weight":"1.20"}]},{"featureType":"administrative.province","elementType":"labels.text.fill","stylers":[{"color":"#b78653"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"hue":"#ffbb00"},{"saturation":54}]},{"featureType":"poi","elementType":"labels","stylers":[{"saturation":-84},{"lightness":6},{"visibility":"off"}]},{"featureType":"poi.attraction","elementType":"all","stylers":[{"saturation":-55}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"},{"saturation":-60},{"gamma":1.91}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"simplified"},{"saturation":-27},{"gamma":1.8}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#ff8800"},{"saturation":-79},{"lightness":-16},{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"saturation":-67},{"hue":"#ff9900"},{"lightness":6},{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#ff6600"},{"saturation":-75},{"lightness":-21}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"saturation":-79},{"visibility":"off"}]},{"featureType":"road.local","elementType":"all","stylers":[{"hue":"#ff5e00"},{"saturation":-24},{"visibility":"simplified"},{"lightness":28}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"transit.line","elementType":"all","stylers":[{"visibility":"simplified"},{"saturation":-27}]},{"featureType":"transit.station","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"saturation":-85}]}]
-}, JMLocations);
+const track = document.querySelector('.downtownMap__track');
+const canvas = document.querySelector('.mapCanvas__frame');
+const filter = document.querySelector('.mapCanvas__filter');
+let menuIsOpen = false;
 
-Downtown._setEvents(Downtown.map, [{
-  event: 'click',
-  action: function() {
-    Downtown.closeIW();
-  }
-}]);
-Downtown.markers.list.map(function(marker) {
-  Downtown._setEvents(marker, [
-    {
-      event: 'click',
-      action: function() {
-        if (Downtown.activeIW === this.infoWindow) {
-          Downtown.closeIW();
-        } else Downtown.openIW(this);
-      }
-    }
-  ]);
+/* MAP MENU APPEARANCE */
+
+filter.addEventListener('click', function(event) {
+  event.preventDefault();
+  if (menuIsOpen) closeMenu();
+  else openMenu();
 });
 
-// Interface.buttons.map(function(button) {
-//   button.addEventListener('click', function() {
-//     let category = this.getAttribute('data-category');
-//     let list = Interface.categories[category].locationList;
-//     if (this.tillstand.active.get()) {
-//       Interface.close(category);
-//       toggleLocations(Interface);
-//       toggleAll();
-//     } else {
-//       Interface.open(category);
-//       toggleByCategory(this);
-//     }
-//     if (list.className.search('has-slimScroll') < 0) {
-//       list.className += '  has-slimScroll';
-//       $(list).slimScroll({
-//         height: '100%',
-//         wheelStep: 10
-//       });
-//     }
-//   });
-// });
+window.addEventListener('resize', function() {
+  if (menuIsOpen && window.innerWidth >= 900) closeMenu(0);
+});
 
-// Interface.locationItems.map(function(location) {
-//   location.addEventListener('click', function() {
-//     if (this.tillstand.active.get()) {
-//       toggleLocations(Interface);
-//       toggleByCategory(this);
-
-//     } else {
-//       toggleLocations(Interface, this);
-//       toggleByLocation(this);
-//     }
-//   });
-// });
-
-// function toggleLocations(Interface, location) {
-//   if (Interface.activeLocation) {
-//     Interface.activeLocation.tillstand.active.set(false);
-//   }
-//   if (location) {
-//     location.tillstand.active.set(true);
-//     Interface.activeLocation = location;
-//   }
-// }
-
-
-function toggleByCategory(element) {
-  let category = element.getAttribute('data-category');
-  Downtown.markers.list.map(function(marker) {
-    if (marker.category === category) {
-      marker.setVisible(true);
-    } else if (!marker.alwaysVisible) {
-      marker.setVisible(false);
-    }
-  });
+function openMenu(duration = 400) {
+  menuIsOpen = true;
+  filter.textContent = "Close";
+  velocity(track, {
+    'translateX': 250
+  },{ duration: duration });
 }
-function toggleByLocation(element) {
-  let name = element.getAttribute('data-name');
-  Downtown.markers.list.map(function(marker) {
-    if (marker.name === name) {
-      if (marker.multiple) {
-        Downtown.zoom(13);
-        Downtown.center(JMLCenter);
-      } else {
-        Downtown.zoom(16);
-        Downtown.center(marker.position);
-      }
-      marker.setVisible(true);
-    } else if (!marker.alwaysVisible) {
-      marker.setVisible(false);
-    }
-  });
-}
-function toggleAll() {
-  Downtown.markers.list.map(function(marker) {
-    marker.setVisible(true);
-  });
+function closeMenu(duration = 400) {
+  menuIsOpen = false;
+  filter.textContent = "Filter";
+  velocity(track, {
+    'translateX': 0
+  },{ duration: duration });
 }
